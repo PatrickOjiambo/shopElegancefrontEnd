9fee66191c4b13b0ad141203a4bd99eeac482ee8
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-09-27 00:26:28 +0300

    Initialize views

--- shopElegance/src/App.jsx
+++ shopElegance/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Header from "./HeaderFooter/Header"
-import Footer from "./HeaderFooter/Footer"
+import Header from "./Reusables/Header"
+import Footer from "./Reusables/Footer"
 function App() {
-    return (<div className="font-light">
+    return (<div className="font-light font-Poppins">
         <Header />
         <Footer />
     </div>);
