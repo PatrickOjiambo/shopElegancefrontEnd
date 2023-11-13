fab23672578a7a3bcb644d7a6b71fc8123892f1e
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-13 21:57:11 +0300

    fix:Fixed login component

--- shopElegance/src/Views/Auth/Login.jsx
+++ shopElegance/src/Views/Auth/Login.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+/**
+ * 
+ * @returns Login Component
+ */
 function Login() {
     const [loginDetails, setLoginDetails] = useState({
         email: '',
@@ -13,6 +17,7 @@ function Login() {
 
     }
     return (<div className="mt-3 flex items-center">
+      
         <div className="">
             <img src="../images/loginImage.png" alt="login Image" className="w-[30rem] h-[25rem]" />
         </div>
@@ -20,10 +25,10 @@ function Login() {
             <p className="text-base">Login to Elegance</p>
             <p className="text-xs mt-2">Enter your details below</p>
             <label className="block mt-3 ">
-                <input value={details.name} onChange={handleChange} placeholder="Enter Email or Phone number" className="border-b focus:outline-none text-xs " />
+                <input value={loginDetails.email} onChange={handleChange} placeholder="Enter Email or Phone number" className="border-b focus:outline-none text-xs " />
             </label>
             <label className="block text-xs mt-3">
-                <input value={details.email} onChange={handleChange} placeholder="Enter Password" className="border-b focus:outline-none text-xs" />
+                <input value={loginDetails.password} onChange={handleChange} placeholder="Enter Password" className="border-b focus:outline-none text-xs" />
 
             </label>
 
@@ -31,7 +36,7 @@ function Login() {
             <div className="flex">
                 <button className="bg-buttonPink text-white text-xs text-center px-3 py-1 rounded-sm mt-2 w-full">Log In</button>
 
-                <p className="inline pl-2 underline text-red-400 hover:text-pink-500">Log In</p>
+                <p className="inline pl-2 underline text-red-400 hover:text-pink-500">Forgot password?</p>
 
             </div>
         </div>
