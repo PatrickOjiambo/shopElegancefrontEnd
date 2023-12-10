0fea10a7f3868d486cb3f537a2175dedc1eccf43
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-12-10 18:47:42 +0300

    feat: Project complete

--- shopElegance/src/Views/Cart/Cart.jsx
+++ shopElegance/src/Views/Cart/Cart.jsx
@@ -1,9 +1,64 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Button from '@mui/material/Button';
+
 function Cart() {
-    
-    return ( <div>
-        
-    </div>);
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
+        const cart = localStorage.getItem("cartItems");
+        if (cart) {
+            setCart(JSON.parse(cart));
+        }
+    }, []);
+
+    return (<div className="">{cart.length != 0  ?<div>
+        <table className="table-auto">
+        <thead className="border-b font-medium dark:border-neutral-500">
+            <tr className="font-Poppins font-extralight ">
+                <th className="px-6 py-4">Product</th>
+             
+                <th className="px-6 py-4">Price</th>
+                <th className="px-6 py-4">Quantity</th>
+                <th className="px-6 py-4">Subtotal</th>
+            </tr>
+        </thead>
+        <tbody>
+            {cart.map((item) => (
+                <tr className="border-b dark:border-neutral-500">
+                    <td className="whitespace-nowrap px-6 py-4">{item.name}</td>
+                    <td className="whitespace-nowrap px-6 py-4">{item.price}</td>
+                    <td className="whitespace-nowrap px-6 py-4">{item.quantity}</td>
+                    <td className="whitespace-nowrap px-6 py-4">{item.price * item.quantity}</td>
+                </tr>
+
+            ))}
+        </tbody>
+    </table>
+    <div className="border-2 border-black p-4 max-w-xl">
+        <h1>Cart Total</h1>
+        <section className="flex justify-between"><p>Subtotal</p>
+            <p>${cart.reduce((total, product) => total + product.price * product.quantity, 0)}</p></section>
+            <section className="flex justify-between">
+                <p>Shipping</p>
+                <p>Free</p>
+            </section>
+        <section className="flex justify-between pb-2">
+            <h1>Total</h1>
+            <p>${cart.reduce((total, product) => total + product.price * product.quantity, 0)}</p>
+        </section>
+        <Button size='large' variant="contained" >Checkout</Button>
+    </div></div> : <div className="flex items-center max-w-3xl bg-slate-300 h-screen p-4">
+        <section>
+        <p className="font-light">Buzzkill alert</p>
+        <h1 className="text-2xl font-Poppins font-semibold pt-2">There is nothing in your cart.</h1>
+        <p className="font-light pt-2 pb-3"> All this stellar refurb isn't gonna shop itself! </p>
+
+        <Button size='large' variant="contained" >Browse sweet deals</Button>
+        </section>
+        <img src="../images/empty-cart.png" className="h-48"/>
+        </div>}
+       
+    </div>)
+        ;
 }
 
 export default Cart;
--- shopElegance/src/Views/Home/item.jsx
+++ shopElegance/src/Views/Home/item.jsx
@@ -1,25 +1,25 @@
-import { useParams, useLoaderData } from 'react-router-dom';
+import { useParams, useLoaderData} from 'react-router-dom';
 import axios from "axios";
 import Rating from '@mui/material/Rating';
 import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
-import {AiOutlineShoppingCart} from "react-icons/ai"
+import { AiOutlineShoppingCart } from "react-icons/ai"
 
-
-const itemLoader = ({params}) =>{
-    return axios.get(`http://localhost:4267/products/${params.product_id}`)
-    .then((res) => {
-      
+/**
+ * 
+ * @param {*} param0 
+ * @returns product object
+ */
+const itemLoader = async ({ params }) => {
+    try {
+        const res = await axios.get(`http://localhost:4267/products/${params.product_id}`);
         return {
             product: res.data
-        }
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-
-}
-
+        };
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 /**
  * 
@@ -27,16 +27,44 @@ const itemLoader = ({params}) =>{
  * @returns Item component - item to be added to cart
  */
 const Item = () => {
+    const params = useParams();
+    const prod = useLoaderData();
+    const [cartItems, setCartItems] = useState(localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []);
+    const [product, setProduct] = useState(null);
 
-    let params = useParams();
-    let todo = useLoaderData();
-    console.log(todo);
-    const [product, setProduct] = useState(todo.product.product);
-    
-    
+    useEffect(() => {
+        setProduct(prod.product.product);
+    }, [prod]);
+
+    /**
+     * 
+     * @param {Object} product 
+     */
+    const handleAddToCart = (product) => {
+        //check if the item is already in the cart
+        const isItemInCart = cartItems.find((cartItem) => cartItem.id === product.id);
+        if (isItemInCart) {
+            //If the item is already in cart, increase the quantity of the item
+            setCartItems(cartItems.map((cartItem) => cartItem.id === product.id ? { ...isItemInCart, quantity: isItemInCart.quantity + 1 } : cartItem))
+        } else {
+            //If the item is not in the cart, add it to the cart
+            setCartItems([...cartItems, { ...product, quantity: 1 }])
+        }
+        console.log("Item added to cart")
+        console.log(cartItems)
+    }
+
+    useEffect(() => {
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+       
+    }, [cartItems])
+
+    if (!product) {
+        return null; // or show a loading spinner
+    }
+
     return (
         <div className='flex justify-between mx-auto p-5 max-w-xl items-center shadow-md '>
-
             <img src={product.image} className='w-auto' />
             <section>
                 <p className='font-bold'>{product.name}</p>
@@ -49,9 +77,53 @@ const Item = () => {
                     <Rating name="read-only" value={product.rating} size="small" readOnly />
                     <p className=" text-zinc-300">(product.comments_count)</p>
                 </div>
-                <Button size='large' variant="contained" startIcon={<AiOutlineShoppingCart/>}>Add to Cart</Button>
+                <Button size='large' variant="contained" startIcon={<AiOutlineShoppingCart />} onClick={() => handleAddToCart(product)}>Add to Cart</Button>
             </section>
-        </div>);
+        </div>
+    );
 }
 
 export { Item, itemLoader };
+//     /**
+//   * 
+//   * @param {Object} product 
+//   */
+//     const handleAddToCart = (product) => {
+//         //check if the item is already in the cart
+//         const isItemInCart = cartItems.find((cartItem) => cartItem.id === product.id);
+//         if(isItemInCart){
+//             //If the item is already in cart, increase the quantity of the item
+//             setCartItems(cartItems.map((cartItem) => cartItem.id === product.id ? {...isItemInCart, quantity: isItemInCart.quantity + 1} : cartItem))
+//         }
+//             else{
+//                 //If the item is not in the cart, add it to the cart
+//                 setCartItems([...cartItems, {...product, quantity: 1}])
+                
+//             }     
+//     }
+//     useEffect(() => {
+//         localStorage.setItem('cartItems', JSON.stringify(cartItems))
+//     }, [cartItems])
+
+
+//     return (
+//         <div className='flex justify-between mx-auto p-5 max-w-xl items-center shadow-md '>
+
+//             <img src={product.image} className='w-auto' />
+//             <section>
+//                 <p className='font-bold'>{product.name}</p>
+//                 <div className="">
+//                     <p className='mt-2'>${product.price}.00</p>
+//                     <p className='text-base mt-4'>{product.description}</p>
+//                 </div>
+//                 <p>In stock</p>
+//                 <div className="flex items-center pb-2">
+//                     <Rating name="read-only" value={product.rating} size="small" readOnly />
+//                     <p className=" text-zinc-300">(product.comments_count)</p>
+//                 </div>
+//                 <Button size='large' variant="contained" startIcon={<AiOutlineShoppingCart />} onClick={handleAddToCart(product)} >Add to Cart</Button>
+//             </section>
+//         </div>);
+// }
+
+// export { Item, itemLoader };
